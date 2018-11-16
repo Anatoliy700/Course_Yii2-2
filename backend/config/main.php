18f55cb27bb68017b4d48f08e213e0026323233a
Anatoliy <anatoliy.rus@gmail.com>
2018-11-16 16:26:18 +0300

    Подключена БД, общая авторизация для backend и frontend, подключен urlManager

--- backend/config/main.php
+++ backend/config/main.php
@@ -19,11 +19,19 @@ return [
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+            'identityCookie' => [
+                'name' => $params['identityCookieName'],
+                'httpOnly' => true,
+                'domain' => $params['cookieDomain']
+            ],
         ],
         'session' => [
             // this is the name of the session cookie used for login on the backend
-            'name' => 'advanced-backend',
+            'name' => $params['sessionCookieName'],
+            'cookieParams' => [
+                'httpOnly' => true,
+                'domain' => $params['cookieDomain']
+            ],
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
@@ -37,14 +45,12 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
